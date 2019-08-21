9f0a3d295c89f03fa83a8d7059379a3c46437bc3
David Thayer <dthayer16@gmail.com>
2019-08-21 14:28:04 -0700

    setting up server, and routes.

---
+++ app/data/friends.js
@@ -0,0 +1,50 @@
+let friendArray = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "David",
+        "photo": "https://avatars0.githubusercontent.com/u/47264947?s=460&v=4",
+        "scores": [
+            5,
+            3,
+            1,
+            2,
+            5,
+            2,
+            4,
+            4,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Mike",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9A-dwH6GcISZvHCqLewy0tbXyX17RZUCaAtbO6TWr0CAC0deSYQ",
+        "scores": [
+            3,
+            2,
+            5,
+            1,
+            2,
+            4,
+            5,
+            5,
+            1,
+            1
+        ]
+    }
+]
